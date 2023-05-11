52acf5077e7b0439f2f1c0719f0c81eb42259852
Marco Ramirez <mvrcentes@hotmail.com>
2023-05-11 10:06:17 -0600

    Add Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
+
+import style from "./Card.module.css"
+
+export const Card = ({ children, to }) => {
+    return (
+        <>
+            {to ? (
+                <Link
+                    to={'/admin/house/' + to}
+                    style={{ textDecoration: "none" }}
+                    className={style.HouseCard}>
+                    {children}
+                </Link>
+            ) : (
+                <div className={style.HouseCard}>{children}</div>
+            )}
+        </>
+    )
+}
+
+Card.propTypes = {
+    data: PropTypes.shape({
+        code: PropTypes.string.isRequired,
+        address: PropTypes.string.isRequired,
+    }),
+}
