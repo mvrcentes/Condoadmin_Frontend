38f64b21b0e8fd64a51f6a94ac066a1bb8c9e3d7
Marco Ramirez <mvrcentes@hotmail.com>
2023-05-11 10:08:01 -0600

    Add admin menu

---
+++ src/components/Menu/AdminMenu.jsx
@@ -0,0 +1,7 @@
+import services from "../../assets/services.svg"
+import houses from "../../assets/houses.svg"
+
+const AdminMenu = [[services, "Servicios", "/admin/services"],
+                   [houses, "Casas", "/admin/houses"]]
+
+export default AdminMenu                   
