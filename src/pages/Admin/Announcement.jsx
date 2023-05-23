a1192549c1d9920cb4ea6963c679e55876b4a98b
Marco Ramirez <mvrcentes@hotmail.com>
2023-05-22 19:01:36 -0600

    Add pages

---
+++ src/pages/Admin/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import AdminView from "../../components/View/AdminView"
+
+import { Annoucements } from "../../CreateViews/Annoucements/Annoucements"
+export const Announcement = () => {
+    return (
+        <AdminView>
+            <Annoucements />
+        </AdminView>
+    )
+}
