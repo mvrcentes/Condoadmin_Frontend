bfa15cd857f864a2d1fdd8b083c35b4d1ca5cce2
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-05-24 19:19:23 -0600

    Add Header to Announcement and Complaints

--- src/pages/Admin/Announcement.jsx
+++ src/pages/Admin/Announcement.jsx
@@ -2,10 +2,16 @@ import React from "react"
 
 import AdminView from "../../components/View/AdminView"
 
+//components
 import { Annoucements } from "../../CreateViews/Annoucements/Annoucements"
+import { Header } from "../../components/Header/Header"
+
+
+
 export const Announcement = () => {
     return (
         <AdminView>
+            <Header />
             <Annoucements />
         </AdminView>
     )
--- src/pages/Admin/Complaint.jsx
+++ src/pages/Admin/Complaint.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import AdminView from '../../components/View/AdminView'
 import { Complaints } from '../../CreateViews/Complaints/Complaints'
+import { Header } from '../../components/Header/Header'
 
 export const Complaint = () => {
   return (
     <AdminView >
+        <Header />
         <Complaints />
     </AdminView>
   )
