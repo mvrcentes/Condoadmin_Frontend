e4b344388f8b4c479dbe53f6f48b0b57b3f7007f
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-09-28 21:11:24 -0600

    feat: update complaints table

--- src/CreateViews/Complaints/Complaints.jsx
+++ src/CreateViews/Complaints/Complaints.jsx
@@ -67,42 +67,54 @@ export const Complaints = ({ search, admin }) => {
         objeto.titulo.toLowerCase().startsWith(search.toLowerCase())
       );
       return (
-        <table className="table-auto w-full bg-blue-100">
-          <thead>
-            <tr>
-              <th className="px-4 py-2 text-left">Titulo</th>
-              <th className="px-4 py-2 text-left">Residente</th>
-              <th className="px-4 py-2 text-left">Fecha</th>
-              <th className="px-4 py-2 text-left">Upvotes</th>
-            </tr>
-          </thead>
-          <tbody>
-            {arrayQuejasFiltradas.map((complaint, index) => (
-              <tr
-                key={index}
-                className="cursor-pointer hover:bg-blue-200"
-                onClick={() => {
-                  // Redirigir a la ruta correspondiente al hacer clic en cualquier dato de la fila
-                  const route = admin
-                    ? `/admin/complaints/${complaint.id}`
-                    : `/resident/complaints/${complaint.id}`;
-                  window.location.href = route;
-                }}
-              >
-                <td className="border px-4 py-2">
-                  <Link
-                    to={admin ? `/admin/complaints/${complaint.id}` : `/resident/complaints/${complaint.id}`}
-                  >
-                    {complaint.titulo}
-                  </Link>
-                </td>
-                <td className="border px-4 py-2">{complaint.residente}</td>
-                <td className="border px-4 py-2">{complaint.fecha}</td>
-                <td className="border px-4 py-2">{complaint.upvotes}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+
+        <div className="container mx-auto py-8">
+          {isLoading ? (
+            <h1 className="text-2xl font-semibold text-center">Loading...</h1>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full bg-white border rounded-lg">
+                <thead className="bg-gray-100">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase">Titulo</th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase">Residente</th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase">Fecha</th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase">Upvotes</th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                  {arrayQuejasFiltradas.map((complaint, index) => (
+                    <tr
+                      key={index}
+                      className="transition-colors cursor-pointer hover:bg-blue-50"
+                      onClick={() => {
+                        const route = admin
+                          ? `/admin/complaints/${complaint.id}`
+                          : `/resident/complaints/${complaint.id}`;
+                        window.location.href = route;
+                      }}
+                    >
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <Link
+                          to={admin ? `/admin/complaints/${complaint.id}` : `/resident/complaints/${complaint.id}`}
+                          className="text-blue-600 hover:underline"
+                        >
+                          {complaint.titulo}
+                        </Link>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">{complaint.residente}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{complaint.fecha}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{complaint.upvotes}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+
+
+
       );
     }
   };
