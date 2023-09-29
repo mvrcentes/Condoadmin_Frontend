a9ef3bf6b2cf44a6c81d4d7ea7dae1a69d8c9901
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-09-28 21:14:08 -0600

    feat: change emergency logo

--- src/components/View/ResidentView.jsx
+++ src/components/View/ResidentView.jsx
@@ -8,12 +8,13 @@ import houses from "../../assets/houses.svg"
 import announce from "../../assets/announce.svg"
 import complaints from "../../assets/complaints.svg"
 import logout from "../../assets/logout.svg"
+import emergencies from "../../assets/emergency.png"
 
 const ResidentMenu = [
   [announce, "Anuncios", "/resident/announce"],
   [complaints, "Quejas", "/resident/complaints"],
   [houses, "Habitantes", "/resident/house/1"],
-  [services, "Emergencias", "/resident/emergencies"],
+  [emergencies, "Emergencias", "/resident/emergencies"],
   [logout, "Salir", "/login", () => {
     sessionStorage.removeItem('token')
   }],
