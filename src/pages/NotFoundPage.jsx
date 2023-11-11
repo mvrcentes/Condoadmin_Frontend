51b83029c8cba56a95f62e3f27e891386e623a0d
isaackeitor <josuem81010@gmail.com>
2023-11-10 19:02:31 -0600

    cambios en textos

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -16,16 +16,16 @@ const NotFoundPage = () => {
       <div className="absolute top-0 left-0 right-0 bottom-0" style={backgroundStyle} />
       <div className="bg-white p-8 rounded shadow-md" style={{ position: 'relative', zIndex: 1 }}>
         <h1 className="text-3xl font-semibold mb-4">
-          Esta página no está disponible
+          Esta página a la que intentas acceder no está disponible
         </h1>
         <p className="text-gray-600">
-          El enlace que seguiste podría estar roto o la página puede haber sido eliminada.
+          El enlace al que intentas acceder podría estar roto o la página puede haber sido eliminada.
         </p>
         <a
           href="/login"
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full block mt-4 text-center transition duration-300 ease-in-out"
         >
-          Regresar al inicio de sesión
+          Volver al inicio de sesión
         </a>
       </div>
     </div>
